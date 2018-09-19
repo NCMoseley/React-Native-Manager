f5f9a83d13c6ea14cd7842588aed97bbd44fe9f4
NCMoseley <ncmoseley@gmail.com>
2018-09-18 17:49:29 -0700

    Add evn file and navigation package

--- src/App.js
+++ src/App.js
@@ -5,19 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 
 import reducers from './reducers/';
-// import config from '../env.js';
+import { config } from '../env.js';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: 'AIzaSyDsElypKztiApRkOt0EJfcvvZY8kfz-h10',
-      authDomain: 'rn-manager-18854.firebaseapp.com',
-      databaseURL: 'https://rn-manager-18854.firebaseio.com',
-      projectId: 'rn-manager-18854',
-      storageBucket: 'rn-manager-18854.appspot.com',
-      messagingSenderId: '463776788246'
-    };
     console.log(config);
     firebase.initializeApp(config);
   }
